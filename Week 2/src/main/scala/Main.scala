01932642a97fe1a4155edb484e460ea88ce30f52
Anna Ogorodova <renita5701@gmail.com>
2021-02-11 23:09:11 +0600

    update

--- Week 2/src/main/scala/Main.scala
+++ Week 2/src/main/scala/Main.scala
@@ -1,13 +1,15 @@
+import java.io.{FileNotFoundException, IOException}
+
 object Hello extends App {
   // type inference
-def ex0(): Unit ={
-  val a = 1
-  val b = "a string"
-  //val c = new Person("Regina")
+  def ex0(): Unit ={
+    val a = 1
+    val b = "a string"
+    //val c = new Person("Regina")
 
-  //val a: Int = 1
-  //val b: String = "a string"
-  //val c: Person = new Person("Regina")
+    //val a: Int = 1
+    //val b: String = "a string"
+    //val c: Person = new Person("Regina")
 }
 
   // Hello
@@ -35,12 +37,12 @@ def ex0(): Unit ={
 
   //match expressions
   def ex4(i:Int): String ={
-//    val result = i match {
-//      case 1 => "one"
-//      case 2 => "two"
-//      case _ => "not 1 or 2"
-//    }
-//    result
+  //   val result = i match {
+  //     case 1 => "one"
+  //     case 2 => "two"
+  //     case _ => "not 1 or 2"
+  //   }
+  //   result
 
     // alternative way
     i match {
@@ -48,15 +50,15 @@ def ex0(): Unit ={
       case 2 => "two"
       case _ => "not 1 or 2"
     }
-
   }
 
+  // yield create for-expressions that yield a result
   def ex5(): Unit ={
     val x = for (i <- 1 to 5) yield i * 2
     println(x)
   }
 
-  // boolean match expression
+// boolean match expression
   def ex6(bool: Boolean): String ={
     val booleanAsString = bool match {
       case true => "true"
@@ -64,21 +66,52 @@ def ex0(): Unit ={
     }
     booleanAsString
   }
-  
-//matching different types
+
+// matching different types
   def getClassAsString(x: Any):String = x match {
     case s: String => s + " is a String"
     case i: Int => "Int"
     case f: Float => "Float"
     case l: List[_] => "List"
-    case p: Person => "Person"
+    //case p: Person => "Person"
     case _ => "Unknown"
   }
 
+// try/catch exceptions
+  def ex7(): Unit ={
+    try {
+      //writeToFile(text)
+    } catch {
+      case fnfe: FileNotFoundException => println(fnfe)
+      case ioe: IOException => println(ioe)
+    }
+  }
 
-  //ex1()
+  // for loops
+  def ex8(): Unit ={
+    for (arg <- args) println(arg)
+
+    // "x to y" syntax
+    for (i <- 0 to 5) println(i)
+
+    // "x to y by" syntax
+    for (i <- 0 to 10 by 2) println(i)
+  }
+
+  // yield and list of strings
+  def ex9(): Unit ={
+    val fruits = List("apple", "banana", "lime", "orange")
+    val fruitLengths = for {
+      f <- fruits
+      if f.length > 4
+    } yield f.length
+    fruitLengths
+  }
+
+  // ex1()
   //print(ex2(2,3))
   //  print(ex3(1, 3))
   //  print(ex4(1))
-  ex5()
+//  ex5()
+  print(ex9())
 }
