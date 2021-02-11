2daf6cfcfc27d9024ecf862d2d7cb88b12ce98e0
Anna Ogorodova <renita5701@gmail.com>
2021-02-11 20:50:47 +0600

    update

--- Week 2/src/main/scala/Main.scala
+++ Week 2/src/main/scala/Main.scala
@@ -1,8 +1,20 @@
 object Hello extends App {
+  // type inference
+def ex0(): Unit ={
+  val a = 1
+  val b = "a string"
+  //val c = new Person("Regina")
 
+  //val a: Int = 1
+  //val b: String = "a string"
+  //val c: Person = new Person("Regina")
+}
+
+  // Hello
   def ex1(): Unit ={
-    val x =  1
-    var y = 1
+    // types of variables
+    val x =  1 // immutable
+    var y = 1 // mutable
     y = y + 1
 
     println(x)
@@ -10,15 +22,18 @@ object Hello extends App {
     println("Hello, world")
   }
 
+  // Int type of method
   def ex2(a:Int, b:Int): Int ={
     a + b
   }
 
+  //  ternary operator
   def ex3(a:Int, b:Int): Int ={
     val x = if (a < b) a else b
     x
   }
 
+  //match expressions
   def ex4(i:Int): String ={
 //    val result = i match {
 //      case 1 => "one"
@@ -27,16 +42,40 @@ object Hello extends App {
 //    }
 //    result
 
+    // alternative way
     i match {
       case 1 => "one"
       case 2 => "two"
       case _ => "not 1 or 2"
     }
+
   }
+
   def ex5(): Unit ={
     val x = for (i <- 1 to 5) yield i * 2
     println(x)
   }
+
+  // boolean match expression
+  def ex6(bool: Boolean): String ={
+    val booleanAsString = bool match {
+      case true => "true"
+      case false => "false"
+    }
+    booleanAsString
+  }
+  
+//matching different types
+  def getClassAsString(x: Any):String = x match {
+    case s: String => s + " is a String"
+    case i: Int => "Int"
+    case f: Float => "Float"
+    case l: List[_] => "List"
+    case p: Person => "Person"
+    case _ => "Unknown"
+  }
+
+
   //ex1()
   //print(ex2(2,3))
   //  print(ex3(1, 3))
